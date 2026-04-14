cd81b45c780d52e46f76260ad5078c4120c33b66
martin2025santos-ops <martin2025santos@gmail.com>
2026-04-14 18:25:15 -0300

    Add files via upload

---
+++ ciclos_ejercicio_1.js
@@ -0,0 +1,10 @@
+//("Hola empezando con mi Trabajo de Ciclos");
+// A) Mostrar números desde 0 hasta 10, avanzando de 3 en 3
+for (let numero = 0; numero <= 10; numero += 3) {
+    console.log(numero);
+}
+
+// B) Mostrar números desde 0 hasta -10, retrocediendo de 3 en 3
+for (let numero = 0; numero >= -10; numero -= 3) {
+    console.log(numero);
+}
